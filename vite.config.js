9688aa1c25236bf64562fccb9bd7299a5dfe5f34
AyamSujud <rasyidcc203@gmail.com>
2024-09-23 09:58:02 +0700

    123

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['SinglePage'], // Replace 'your-module-name' with the actual module you want to externalize
+    },
+  },
+});
